042407b32f3c1f15459b54deb7f30cf54a0ebe5d
Your Name <jann.chia.2020@scis.smu.edu.sg>
2022-09-22 23:33:18 +0800

    Added foreign key for role_required_skill table

---
+++ flask/role_required_skill.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+from ljps_role import ljps_role
+from skill import Skill
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root' + \
+                                        '@localhost:3306/is212'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
+                                           'pool_recycle': 280}
+
+db = SQLAlchemy(app)
+
+CORS(app)
+
+class role_required_skill(db.Model):
+    __tablename__ = 'role_required_skill'
+
+    ljpsr_id = db.Column(db.Integer, db.ForeignKey(ljps_role.ljpsj_id), primary_key=True)
+    skill_id = db.Column(db.Integer,  db.ForeignKey(Skill.skill_id),primary_key=True)
