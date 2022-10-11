3a61844fe967ab5d7bd03183af58181dcd85b1a2
Ji1en <kelvin.yap.2020@scis.smu.edu.sg>
2022-10-11 14:21:36 +0800

    Added login route

--- ui/js/login.js
+++ ui/js/login.js
@@ -6,8 +6,37 @@
 function login(){
     var staff_id = document.getElementById('staff_ID').value;
     sessionStorage.setItem('staff_id', staff_id);
-    var staff_role = // 1/2/3/4; 
-    sessionStorage.setItem('staff_role', staff_role);
+    $(async () => {
+        var serviceURL = "http://127.0.0.1:5001/login/" + staff_id
+        try {
+            const response =
+                await fetch(
+                serviceURL, { mode: 'cors', method: 'GET' }
+            );
+            // console.log(response)
+            const result = await response.json();
+            // console.log(result.data)
+            if(result) {
+                console.log('User data retrieved')
+                new_lj_details = JSON.stringify(result.data)
+                var staff_role = result.data.role_id;
+                // console.log(staff_role);
+                var f_name = result.data.staff_fname
+                var l_name = result.data.staff_lname
+                var full_name = f_name + " " + l_name
+                var dept = result.data.dept
+                sessionStorage.setItem('staff_role', staff_role)
+                sessionStorage.setItem('full_name', full_name)
+                sessionStorage.setItem('dept', dept)
+
+
+                
+                }
+        } catch (error) {
+            console.log(error)
+            console.log('error')
+        }
+    })
 }
 
 // BACKEND TO USE ^ staff_id TO TELL FRONTEND USER DETAILS
