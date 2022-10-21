982d119b450ccb731a88cbd0542e7f03c4192004
rhyun04 <rhys.tan.2020@scis.smu.edu.sg>
2022-10-21 12:42:56 +0800

    Notification fix
    Frontend

--- ui/js/dashboard_standard.js
+++ ui/js/dashboard_standard.js
@@ -2,6 +2,8 @@ window.onload = function(){
     if(!window.location.hash) {
 		window.location = window.location + '#loaded';
 		window.location.reload();
+        var message = 'The learning journey has been updated.'
+        localStorage.setItem('errmessage', message);
 	}
 }
 
