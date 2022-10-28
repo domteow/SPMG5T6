4c76bc3c30f626978da1fe202c2a4690bb8fe833
domteow <dominicteow@gmail.com>
2022-10-28 18:24:59 +0800

    Update delete_LJ.js

--- ui/js/delete_LJ.js
+++ ui/js/delete_LJ.js
@@ -1,20 +1,31 @@
 async function deleteLJ(deleteid){
-    var lj_id = deleteid.split('/')[1];
-    console.log(lj_id);
+    var userinput = prompt('Are you sure you want to delete this learning journey? (Y/N)')
 
-    //  delete LJ 
-    var serviceURL = "http://127.0.0.1:5001/delete_LJ/"
-
-    try {
-        const response = await fetch(serviceURL,
-            { mode: 'cors', method: ['DELETE'],
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({
-                "journey_id" : lj_id
-            })});
-
-        console.log(response)
-    } catch (error) {
-        
+    if(userinput.toLowerCase() == 'y') {
+        var lj_id = deleteid.split('/')[1];
+        console.log(lj_id);
+    
+        //  delete LJ 
+        var serviceURL = "http://127.0.0.1:5001/delete_LJ/"
+    
+        try {
+            const response = await fetch(serviceURL,
+                { mode: 'cors', method: ['DELETE'],
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({
+                    "journey_id" : lj_id
+                })});
+    
+            console.log(response)
+            if(response.status === 201 || response.status === 200) {
+                console.log('Learning Journey deleted')
+                alert('Learning Journey deleted')
+                
+            }
+        } catch (error) {
+            console.log(error)
+            console.log('error')
+        }
     }
+
 }
