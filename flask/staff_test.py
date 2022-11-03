c4404888cdaa260cd99e5760fc63bc0390085365
brunogjh <89073108+brunogjh@users.noreply.github.com>
2022-11-03 17:12:38 +0800

    Completed unit tests for staff class

--- flask/staff_test.py
+++ flask/staff_test.py
@@ -15,7 +15,11 @@ class TestStaff(flask_testing.TestCase):
     def setUp(self):
         db.create_all()
         self.s1 = Staff(staff_id = 130001, staff_fname = 'John', staff_lname = 'Sim', dept = 'Chariman', email = 'jack.sim@allinone.com.sg', role_id = 1)
+        self.s2 = Staff(staff_id = 140001, staff_fname = 'Derek', staff_lname = 'Tan', dept = 'Sales', email = 'Derek.Tan@allinone.com.sg', role_id = 3)
+        self.s3 = Staff(staff_id = 140002, staff_fname = 'Susan', staff_lname = 'Goh', dept = 'Sales', email = 'Susan.Goh@allinone.com.sg', role_id = 2)
         db.session.add(self.s1)
+        db.session.add(self.s2)
+        db.session.add(self.s3)
         db.session.commit()
 
     def tearDown(self):
@@ -23,7 +27,6 @@ class TestStaff(flask_testing.TestCase):
         db.drop_all()
 
     def test_to_dict(self):
-        # s1 = Staff(staff_id = 130001, staff_fname = 'John', staff_lname = 'Sim', dept = 'Chariman', email = 'jack.sim@allinone.com.sg', role_id = 1)
         self.assertEqual(self.s1.to_dict(), {
             "staff_id": 130001,
             "staff_fname": 'John',
@@ -43,7 +46,52 @@ class TestStaff(flask_testing.TestCase):
             "role_id": 1
         })
     def test_get_staff_from_department(self):
-        pass
+        self.assertEqual(Staff.get_staff_from_department('Sales'),[
+            {
+            "staff_id": 140001,
+            "staff_fname": 'Derek',
+            "staff_lname": 'Tan',
+            "dept": 'Sales',
+            "email": 'Derek.Tan@allinone.com.sg',
+            "role_id": 3
+        },
+        {
+            "staff_id": 140002,
+            "staff_fname": 'Susan',
+            "staff_lname": 'Goh',
+            "dept": 'Sales',
+            "email": 'Susan.Goh@allinone.com.sg',
+            "role_id": 2
+        }
+        ])
+    
+    def test_get_all_staff(self):
+        self.assertEqual(Staff.get_all_staff(),[
+            {
+            "staff_id": 130001,
+            "staff_fname": 'John',
+            "staff_lname": 'Sim',
+            "dept": 'Chariman',
+            "email": 'jack.sim@allinone.com.sg',
+            "role_id": 1
+        },
+        {
+            "staff_id": 140001,
+            "staff_fname": 'Derek',
+            "staff_lname": 'Tan',
+            "dept": 'Sales',
+            "email": 'Derek.Tan@allinone.com.sg',
+            "role_id": 3
+        },
+        {
+            "staff_id": 140002,
+            "staff_fname": 'Susan',
+            "staff_lname": 'Goh',
+            "dept": 'Sales',
+            "email": 'Susan.Goh@allinone.com.sg',
+            "role_id": 2
+        }
+        ])
 
 if __name__ == "__main__":
     unittest.main()  
